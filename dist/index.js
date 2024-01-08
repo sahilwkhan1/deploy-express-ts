67aa57bd3fe08dcc699ac044bf445400b2f6f3b4
G7CR Sahil Khan <sahil.khan@g7cr.com>
2024-01-08 18:46:19 +0530

    used mongodb

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,56 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongodb_1 = require("mongodb");
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
+// MongoDB connection URI
+const DB_URL = 'mongodb+srv://g7crservicesdev:tB3q2YrncrGugTIh@lms.allbqqh.mongodb.net/lms?retryWrites=true&w=majority';
+let db = null;
+function connectToDatabase() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const client = new mongodb_1.MongoClient(DB_URL);
+        try {
+            yield client.connect();
+            console.log('Connected to MongoDB');
+            db = client.db('lms');
+        }
+        catch (error) {
+            console.error('Error connecting to MongoDB:', error);
+            process.exit(1);
+        }
+    });
+}
+connectToDatabase();
 app.get('/', (_req, res) => {
-    return res.send('Express Typescript on Vercel');
+    return res.send('Express TypeScript on Vercel');
 });
 app.get('/ping', (_req, res) => {
     return res.send('pong 🏓');
 });
+app.get('/user', (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const users = yield db.collection('users').find().toArray();
+        return res.json(users);
+    }
+    catch (error) {
+        console.error('Error fetching users:', error);
+        return res.status(500).send('Internal Server Error');
+    }
+}));
 app.listen(port, () => {
-    return console.log(`Server is listening on ${port}`);
+    console.log(`Server is listening on ${port}`);
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,49 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express';
+import { MongoClient } from 'mongodb';
 
-const app = express()
-const port = process.env.PORT || 8080
+const app = express();
+const port = process.env.PORT || 8080;
+
+// MongoDB connection URI
+const DB_URL = 'mongodb+srv://g7crservicesdev:tB3q2YrncrGugTIh@lms.allbqqh.mongodb.net/lms?retryWrites=true&w=majority';
+
+import { Db } from 'mongodb';
+
+let db: Db | null = null;
+
+async function connectToDatabase() {
+  const client = new MongoClient(DB_URL );
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+    db = client.db('lms');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
+}
+
+
+connectToDatabase();
 
 app.get('/', (_req: Request, res: Response) => {
-  return res.send('Express Typescript on Vercel')
-})
+  return res.send('Express TypeScript on Vercel');
+});
 
 app.get('/ping', (_req: Request, res: Response) => {
-  return res.send('pong 🏓')
-})
+  return res.send('pong 🏓');
+});
+
+app.get('/user', async (_req: Request, res: Response) => {
+  try {
+    const users = await db.collection('users').find().toArray();
+    return res.json(users);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    return res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(port, () => {
-  return console.log(`Server is listening on ${port}`)
-})
+  console.log(`Server is listening on ${port}`);
+});
